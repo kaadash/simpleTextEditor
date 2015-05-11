084d017dcc0acf8907f9e882054b1a4f82818597
kaadash <arturkudel@gmail.com>
2015-05-11 12:03:39 +0200

    changed choosing style from list to select mode, clean css, improve choosing font

--- js/view/menuView.js
+++ js/view/menuView.js
@@ -1,11 +1,11 @@
 var StyleInputView = Backbone.View.extend({
-	tagName: 'li',
-	className: 'list',
+	tagName: 'option',
+	// className: 'list',
 	template: _.template($('#menu-template').html()),
-	events: {
-		'click': 'changeStyle',
+	// events: {
+	// 	'click': 'changeStyle',
 
-	},
+	// },
 	initialize: function() {
 
 		this.render();
@@ -13,20 +13,24 @@ var StyleInputView = Backbone.View.extend({
 	render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
-	},
-	changeStyle: function(){	
-		var styledText = this.model.get('typeStart') + window.globalText + this.model.get('typeEnd');	
-		var fullText = $('#text-input').val().toString();
-		var changedText = fullText.replace(window.globalText, styledText);
-		window.changedText = changedText;	
-		console.log(window.changedText);		
 	}
+	// changeStyle: function(){	
+	// 	var styledText = this.model.get('typeStart') + window.globalText + this.model.get('typeEnd');	
+	// 	var fullText = $('#text-input').val().toString();
+	// 	var changedText = fullText.replace(window.globalText, styledText);
+	// 	window.changedText = changedText;	
+	// 	console.log(window.changedText);		
+	// }
 });
 
 var ListStyleView = Backbone.View.extend({
-	tagName: 'ul',
-	className: 'menu',
+	tagName: 'select',
+	// className: 'menu',
 	template: _.template($('#input-template').html()),
+	events: {
+		'change': 'changeStyle',
+
+	},
 	initialize: function(){
 		this.render();
 	},
@@ -37,6 +41,19 @@ var ListStyleView = Backbone.View.extend({
 			this.$el.append(styleInputView.render().el);
 		},this);
 		return this; 
+	},
+	changeStyle: function(e){
+		var textOption = e.currentTarget.value;
+		for (var i = this.collection.length - 1; i >= 0; i--) {
+			if(this.collection.at(i).get('name') === textOption) {
+				var thatModel = this.collection.at(i);
+				var styledText = thatModel.get('typeStart') + window.globalText + thatModel.get('typeEnd');	
+				var fullText = $('#text-input').val().toString();
+				var changedText = fullText.replace(window.globalText, styledText);
+				window.changedText = changedText;	
+				console.log(window.changedText);				
+			}
+		};
 	}
 });
 
@@ -77,7 +94,7 @@ var FontsView = Backbone.View.extend({
 		if(text.length === 0) {
 			window.textLength = 0;
 		}
-		if(e.keyCode!==16 && e.keyCode!==8) {
+		if(e.keyCode!==16 && e.keyCode!==8 && e.keyCode!==13) {
 			window.textLength++;			
 		}
 		else if(e.keyCode===8 && window.textLength !== 0){
@@ -94,7 +111,7 @@ var FontsView = Backbone.View.extend({
 			for (var j = 0; j < this.collection.length; j++) {
 				if(this.collection.at(j).get('fontName') !== matchingModels[i]){
 					this.collection.at(j).set('selected', false);
-					console.log(this.collection.at(j).get('fontName')+ ' '+j);		
+					// console.log(this.collection.at(j).get('fontName')+ ' '+j);		
 				}
 				else {
 					indexOfModels.push(j);
@@ -105,10 +122,22 @@ var FontsView = Backbone.View.extend({
 		for (var i = indexOfModels.length - 1; i >= 0; i--) {
 			this.collection.at(indexOfModels[i]).set('selected', true);
 		};
+		if(e.keyCode === 13 && indexOfModels.length === 1) {
+			var thatModel = this.collection.at(indexOfModels);
+			$('.font-input').val(thatModel.get('fontName'));
+			var fullText = $('#text-input').val().toString();
+			var textToChange = window.globalText + thatModel.get('startTag') + thatModel.get('endTag');
+			var changedText = fullText.replace(window.globalText, textToChange);
+			window.changedText = changedText;	
+			// var SearchInput = $('.text-input');
+			// var strLength = SearchInput.val().length;
+			// SearchInput.focus();
+			// SearchInput[0].setSelectionRange(strLength-2, strLength-2);
+		}
 		if(text.length !== 0 && indexOfModels.length === 0) {
 			this.settingModelBool(false);
 		}
-		if(text.length === 0){
+		else if(text.length === 0){
 			this.settingModelBool(true);			
 		}
 	}
